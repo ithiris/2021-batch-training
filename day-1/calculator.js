d3ab7d21183386e6829dd94cd1e350d8ad997352
ithiris <ithiris@provility.com>
2021-07-28 16:03:58 +0530

    added exe1

--- day-1/calculator.js
+++ day-1/calculator.js
@@ -1,6 +1,28 @@
 const {inputInt, inputText, output} = require('./input-output');
-const answer = inputText(" What is your name ?" );
-output(answer);
+
+
+// // with hard coding//
+// const a = 35;
+//  const b = 2;
+//   c = a * b; 
+// output(c);
+
+// //without hardcoding // using prompt user enter the value via terminal //
+// const a = inputInt("Enter Value for a"); 
+// const b = inputInt("Enter Value for b"); 
+// c = a * b;
+//  output(c);
+
+
+ //EXE 1
+ const a = inputInt("Enter Value for a");
+  const b = inputInt("Enter Value for b"); 
+  const operatorCode = inputText("Enter operator code"); 
+  let c = null;
+//hint condition use
+// if a value 10 b value 10 output c will be 20 condition fail c value is null
+
+
 
 
 
