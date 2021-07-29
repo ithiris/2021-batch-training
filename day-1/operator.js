ada72838ee3c3388951331e9e7862682843216e5
Vali032 <valiyullah@provility.com>
2021-07-29 01:15:42 -0700

    implemented if condition statement

---
+++ day-1/operator.js
@@ -0,0 +1,17 @@
+const {inputInt, inputText, output} = require('./input-output');
+
+
+
+const a = inputInt("Enter Value for a");
+  const b = inputInt("Enter Value for b"); 
+  const operatorCode = inputText("Enter operator code"); 
+  let c = null;
+  if(operatorCode == '+') 
+  {
+   c = a + b;
+   }
+  if(operatorCode == '-') 
+  {
+   c = a - b; 
+  }
+  output(c);
