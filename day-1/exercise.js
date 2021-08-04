71c70cab5c979f7e7771c6fc88e88f897a9c601c
Vali032 <valiyullah@provility.com>
2021-08-04 03:46:06 -0700

    exercise.js

--- day-1/exercise.js
+++ day-1/exercise.js
@@ -1,7 +1,11 @@
-const fill = (arraySize, value) => {
-    for ( i = 1; i <= 3; i++)
-    console.log(valueToFill);
-};
-const data = 3;
+const data = 5;
 const valueToFill = 'a';
-fill(data, valueToFill);
+const fill = (arraySize, value) => {
+    const result = [];
+    for (let i = 1; i <= arraySize; i++) {
+        result.push(value);
+        console.log(result);
+    }
+}
+fill(5, 'a');
+fill(4, 'b');
