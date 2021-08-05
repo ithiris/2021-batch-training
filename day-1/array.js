170983f9e489ffa50bc800737bb4be986a39f937
Vali032 <valiyullah@provility.com>
2021-08-05 00:12:59 -0700

    executed array task

--- day-1/array.js
+++ day-1/array.js
@@ -1,16 +1,28 @@
-var array =[1,2,3,4,5];
-var arraytwo =[8,9,10,11];
+// var array =[1,2,3,4,5];
+// var arraytwo =[8,9,10,11];
 
- function arrayconcat(inputarray,inputarraytwo)
- {
-     return inputarray.concat(inputarraytwo);
- }
- console.log(arrayconcat(array,arraytwo));
-
-
+//  function arrayconcat(inputarray,inputarraytwo)
+//  {
+//      return inputarray.concat(inputarraytwo);
+//  }
+//  console.log(arrayconcat([1,2,3,4,5], [8,9,10,11]));
 
 
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [8, 9, 10, 11];
+function mergearray (array, array2) {
+    const combined = [];
 
+for (let i = 0; i < array.length; i++) 
+{
+    combined.push(array[i]);
+}
+  for (let j = 0; j < array2.length; j++) 
+  {
+    combined.push(array2[j]); 
+  }
+  return combined;
+}
 
-
+console.log(mergearray(arr1, arr2));
 
