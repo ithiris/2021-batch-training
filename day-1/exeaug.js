2c0c0ace642d3ed245a2d15d727707e3a3a218cd
Vali032 <87024695+Vali032@users.noreply.github.com>
2021-08-13 21:11:03 +0530

    implemented exercise

---
+++ day-1/exeaug.js
@@ -0,0 +1,28 @@
+//exe 1
+
+for (let i=0; i<=20; i++) 
+    {
+   if(i%2==1) 
+   console.log(i);
+    }
+    
+    
+    //exe 2
+
+    for (let i=20; i>=1; i--) 
+    {
+        console.log(i)
+    }
+
+    // //exe 3
+
+        function test() {
+  
+            for(let i=1; i<=10; i++) {
+               
+              console.log("9 * " + i + ' = ' + (9 * i))
+            //   console.log(i)
+            
+            }
+            }
+            test();
